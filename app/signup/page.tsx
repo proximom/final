c5259c02173bcdbb9b9dccf5697694e23000f548
proximom <ahsan.farooq531@gmail.com>
2024-06-06 11:55:10 +0500

    finalChanges

---
+++ app/signup/page.tsx
@@ -0,0 +1,12 @@
+export default function (){
+    return(
+        <div>
+            <h1>
+                    awdwa   
+            </h1>
+            <h2>
+                    wadwa
+            </h2>
+        </div>
+    )
+}
